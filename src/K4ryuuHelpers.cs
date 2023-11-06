262245dd9401bd17c92ab27c043d14096877c2aa
K4ryuu <104531589+K4ryuu@users.noreply.github.com>
2023-11-06 12:23:03 +0100

    feat: Random helper functions added

--- src/K4ryuuHelpers.cs
+++ src/K4ryuuHelpers.cs
@@ -19,4 +19,29 @@ internal static class K4ryuu
 	{
 		return Utilities.FindAllEntitiesByDesignerName<CCSGameRulesProxy>("cs_gamerules").First().GameRules!;
 	}
+
+	internal static CCSPlayerResource PlayerResource()
+	{
+		return Utilities.FindAllEntitiesByDesignerName<CCSPlayerResource>("cs_player_manager").First();
+	}
+
+	internal static IEnumerable<CCSTeam> TeamManagers()
+	{
+		return Utilities.FindAllEntitiesByDesignerName<CCSTeam>("cs_team_manager");
+	}
+
+	internal static CCSTeam? TeamManager(CsTeam team)
+	{
+		IEnumerable<CCSTeam> teamManagers = TeamManagers();
+
+		foreach (var teamManager in teamManagers)
+		{
+			if ((int)team == teamManager.TeamNum)
+			{
+				return teamManager;
+			}
+		}
+
+		return null;
+	}
 }
